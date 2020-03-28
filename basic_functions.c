0564d62810707f5932ee1480568f4667abe430b7
Apurva <58025903+apurvagurme@users.noreply.github.com>
2020-03-28 22:02:11 +0530

    Added two basic functions and modified previous.
    Added convert_fahrentheit_to_centigrade and
    convert_centigrade_to_fahrenheit functions also modified the return
    types of some previous functions.

--- basic_functions.c
+++ basic_functions.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
 
-int is_even(int);
-int is_odd(int);
+unsigned char is_even(int);
+unsigned char is_odd(int);
 int cube(int);
+int square(int);
 int simple_interest(int, int, int);
-int greatest_of_three(int, int, int);
-int average_of_three(int, int, int);
+double convert_fahrenheit_to_centigrade(float);
+double convert_centigrade_to_fahrenheit(float);
+float greatest_of_three(float, float, float);
+float average_of_three(float, float, float);
 
-int is_even(int number)
+unsigned char is_even(int number)
 {
   return number % 2 == 0;
 }
 
-int is_odd(int number)
+unsigned char is_odd(int number)
 {
   return is_even(number) ? 0 : 1;
 }
@@ -32,28 +35,39 @@ int simple_interest(int principle, int rate, int time)
   return (principle * rate * time) / 100;
 }
 
-int greatest_of_three(int num1, int num2, int num3)
+double convert_fahrenheit_to_centigrade(float fahrenheit)
 {
-  if(num1 > num2 && num1 > num3)
-  {
-    return num1;
-  }
-  if(num2 > num1 && num2 > num3)
-  {
-    return num2;
-  }
-  return num3;
+  return (fahrenheit - 32) * 5 / 9;
 }
 
-int average_of_three(int num1, int num2, int num3)
+double convert_centigrade_to_fahrenheit(float celsius)
+{
+  return (celsius * 9 / 5) + 32;
+}
+
+float greatest_of_three(float num1, float num2, float num3)
+{
+  float greatest = num1;
+  if(num2 > num1)
+  {
+    greatest = num2;
+  }
+  if(num3 > num2)
+  {
+    greatest = num3;
+  }
+  return greatest;
+}
+
+float average_of_three(float num1, float num2, float num3)
 {
   return (num1 + num2 + num3) / 3;
 }
 
 int main(void)
 {
-  int num, num1,num2, num3, principle, rate, time;
-
+  int num, principle, rate, time;
+  float num1, num2, num3, temp;
   printf("Enter a number for isEven: ");
   scanf("%d",&num);
   is_even(num) ? printf("%d is an even number\n",num) : printf("%d is not an even number\n",num);
@@ -79,17 +93,25 @@ int main(void)
   scanf("%d",&time);
   printf("Simple interest is: %d\n",simple_interest(principle, rate, time));
 
+  printf("Enter a number for convert_fahrenheit_to_centigrade: ");
+  scanf("%f",&temp);
+  printf("centigrade temp of the fahrenheit temp is: %lf\n",convert_fahrenheit_to_centigrade(temp));
+
+  printf("Enter a number for convert_centigrade_to_fahrenheit: ");
+  scanf("%f",&temp);
+  printf("fahrenheit temp of the centigrade temp is: %lf\n",convert_centigrade_to_fahrenheit(temp));
+
   printf("Enter three numbers for greatest of three: ");
-  scanf("%d",&num1);
-  scanf("%d",&num2);
-  scanf("%d",&num3);
-  printf("Greatest number is: %d\n",greatest_of_three(num1, num2, num3));
+  scanf("%f",&num1);
+  scanf("%f",&num2);
+  scanf("%f",&num3);
+  printf("Greatest number is: %f\n",greatest_of_three(num1, num2, num3));
 
   printf("Enter three numbers for average: ");
-  scanf("%d",&num1);
-  scanf("%d",&num2);
-  scanf("%d",&num3);
-  printf("Average is: %d\n",average_of_three(num1, num2, num3));
+  scanf("%f",&num1);
+  scanf("%f",&num2);
+  scanf("%f",&num3);
+  printf("Average is: %f\n",average_of_three(num1, num2, num3));
 
   return 0;
 }
