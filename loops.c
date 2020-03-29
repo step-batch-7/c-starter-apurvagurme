95c9ec7a1bada13be945c037074dbf8550c7a788
Apurva <58025903+apurvagurme@users.noreply.github.com>
2020-03-29 20:41:39 +0530

    Added loop functions in the loops.c file.

--- loops.c
+++ loops.c
@@ -2,6 +2,15 @@
 
 int factorial(int);
 int fibonacci(int);
+int odd_numbers_from_1(int);
+int even_numbers(int);
+int multiplication_table(int, int);
+int sum_of_n_numbers(int, int);
+int product_of_n_numbers(int, int);
+int odd_numbers_from_n(int, int);
+int every_nth_number(int, int, int);
+int sum_of_even_numbers(int, int);
+int odd_numbers_in_reverse(int);
 
 int factorial(int number)
 {
@@ -36,17 +45,173 @@ int fibonacci(int num)
     return 0;
 }
 
+int odd_numbers_from_1(int edge)
+{ 
+  for(int index = 1; index <= edge; index++)
+  {
+    if(index % 2 != 0)
+    {
+    printf("%d\n", index);
+    }
+  }
+  return 0;
+}
+
+int even_numbers(int edge)
+{ 
+  for(int index = 1; index <= edge; index++)
+  {
+    if(index % 2 == 0)
+    {
+    printf("%d\n", index);
+    }
+  }
+  return 0;
+}
+
+int multiplication_table(multiplier, upto)
+{
+  for(int index = 1; index <= upto; index++)
+  {
+    int multiplication = multiplier * index;
+    printf("%d * %d = %d\n", multiplier, index, multiplication);
+  }
+  return 0;
+}
+
+int sum_of_n_numbers(from, to)
+{
+  int sum = 0;
+  for(int index = from; index <= to; index++)
+  {
+    sum = sum + index;
+  }
+  return sum;
+}
+
+int product_of_n_numbers(from, to)
+{
+  int product = 1;
+  for(int index = from; index <= to; index++)
+  {
+    product = product * index;
+  }
+  return product;
+}
+
+int odd_numbers_from_n(from, to)
+{
+  for(int index = from; index <= to; index++)
+  {
+    if(index % 2 != 0)
+    {
+      printf("%d\n", index);
+    }
+  }
+  return 0;
+}
+
+int every_nth_number(difference, from, to)
+{
+  int nth_number = from;
+  printf("%d\n", nth_number);
+  for(int index = from; index <= to; index++)
+  {
+    nth_number = nth_number + difference;
+    if(nth_number <=to)
+    {
+      printf("%d\n", nth_number);
+    }
+  }
+  return 0;
+}
+
+int sum_of_even_numbers(from, to)
+{
+  int sum;
+  for(int index = from; index <= to; index++)
+  {
+    if(index % 2 == 0)
+    {
+      sum = sum + index;
+    }
+  }
+  return sum;
+}
+
+int odd_numbers_in_reverse(int nth_number)
+{
+  for(int index = nth_number; index > 0; index--)
+  {
+    if(index % 2 != 0)
+    {
+      printf("%d\n", index);
+    }
+  }
+  return 0;
+}
+
 int main(void)
 {
-  int number;
+  int number, number1, number2, difference;
 
   printf("Enter a number for factorial: ");
   scanf("%d",&number);
-  printf("Factorial of a number is: %d", factorial(number));
+  printf("Factorial of a number is: %d\n", factorial(number));
 
   printf("Enter a number for fibonacci: ");
   scanf("%d",&number);
   fibonacci(number);
 
+  printf("Enter a number for odd_numbers_from_1: ");
+  scanf("%d",&number);
+  odd_numbers_from_1(number);
+
+  printf("Enter a number for even_numbers: ");
+  scanf("%d",&number);
+  even_numbers(number);
+
+  printf("Enter multiplier: ");
+  scanf("%d",&number1);
+  printf("Enter boundary: ");
+  scanf("%d",&number2);
+  multiplication_table(number1, number2);
+
+  printf("Enter starting number: ");
+  scanf("%d",&number1);
+  printf("Enter nth number: ");
+  scanf("%d",&number2);
+  printf("Sum of the numbers is: %d\n",sum_of_n_numbers(number1, number2));
+
+  printf("Enter starting number: ");
+  scanf("%d",&number1);
+  printf("Enter nth number: ");
+  scanf("%d",&number2);
+  printf("Product of the numbers is: %d\n",product_of_n_numbers(number1, number2));
+
+  printf("Enter starting number: ");
+  scanf("%d",&number1);
+  printf("Enter nth number: ");
+  scanf("%d",&number2);
+  odd_numbers_from_n(number1, number2);
+
+  printf("Enter nth difference: ");
+  scanf("%d",&difference);
+  printf("Enter starting number: ");
+  scanf("%d",&number1);
+  printf("Enter nth number: ");
+  scanf("%d",&number2);
+  every_nth_number(difference, number1, number2);
+  
+  printf("Enter starting number: ");
+  scanf("%d",&number1);
+  printf("Enter nth number: ");
+  scanf("%d",&number2);
+  printf("Sum of the even numbers is: %d\n",sum_of_even_numbers(number1, number2));
+  
+  printf("Enter nth number: ");
+  scanf("%d",&number);
+  odd_numbers_in_reverse(number);
+  
   return 0; 
 }
